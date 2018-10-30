b4ef9f404411a454a390ab78423250cc2ec3aac2
Tom Anderson <dante.renevo@gmail.com>
2018-10-30 06:45:51 -0700

    udpates

--- go.mod
+++ go.mod
@@ -1,3 +1,1 @@
 module github.com/renevo/go-prefixing/v1
-
-require github.com/ryanuber/go-glob v0.0.0-20170128012129-256dc444b735
--- tree.go
+++ tree.go
@@ -1,7 +1,3 @@
-// Copyright 2013 Julien Schmidt. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be found
-// in the LICENSE file.
-
 package prefixing
 
 import (
